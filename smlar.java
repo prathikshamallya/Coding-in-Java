a9c4367d147b7d188fe2b5e56150332142863e67
PrathikshaMallya <prathikshamallya12@gmail.com>
2024-03-04 21:01:21 +0530

    max min

---
+++ smlar.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class smlar {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        System.out.println("enter the no of elements in an array");
+        n=sc.nextInt();
+        int[] a=new int[n];
+        System.out.println("enter array elements");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+       }
+       int min,max;
+       if(n==1){
+        min=a[0];
+        max=a[0];
+        System.out.println("min is:"+min);
+        System.out.println("max is:"+max);
+       }
+       else if(n==2){
+        min=a[0];
+        max=a[0];
+        if(a[1]>max){
+            min=a[1];
+            max=a[0];
+        }else{
+            min=a[0];
+            max=a[1];
+        }
+        System.out.println("min is:"+min);
+        System.out.println("max is:"+max);
+        }
+        else{
+            min=a[0];
+            max=a[0];
+            for(i=1;i<n;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+            if(a[i]<min){
+                min=a[i];
+            }}
+            System.out.println("min is:"+min);
+            System.out.println("max is:"+max);
+        }
+    }
+}
