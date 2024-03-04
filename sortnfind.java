2b42f4b6cb87397a08e5e588652e4401e5d0fa19
PrathikshaMallya <prathikshamallya12@gmail.com>
2024-03-04 21:27:40 +0530

    max-min with sort

---
+++ sortnfind.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class sortnfind {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        System.out.println("Enter the no of elements in an array: ");
+        n=sc.nextInt();
+        int[] a=new int[n];
+        System.out.println("Enter array elements: ");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+       }
+       System.out.println("The array before sorting is: ");
+       for(i=0;i<n;i++)
+       {
+        System.out.print(" "+a[i]);
+       }
+       //sorting
+
+       for(i=0;i<n-1;i++)
+       {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+       }
+       System.out.println("The sorted array is: ");
+       for(i=0;i<n;i++)
+       {
+        System.out.print(" "+a[i]);
+       }
+       System.out.println();
+       System.out.println("the Maximum and the Minimum elements in the sorted array are");
+       System.out.println("Min is: "+a[0]);
+       System.out.println("Max is: "+a[n-1]);
+    }
+}
