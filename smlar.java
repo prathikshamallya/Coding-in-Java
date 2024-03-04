af60645e91ba551ea8f00f344299fc4985f8106c
PrathikshaMallya <prathikshamallya12@gmail.com>
2024-03-04 21:24:12 +0530

    final commit

--- smlar.java
+++ smlar.java
@@ -4,19 +4,20 @@ public class smlar {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n,i;
-        System.out.println("enter the no of elements in an array");
+        System.out.println("Enter the no of elements in an array: ");
         n=sc.nextInt();
         int[] a=new int[n];
-        System.out.println("enter array elements");
+        System.out.println("Enter array elements: ");
         for(i=0;i<n;i++){
             a[i]=sc.nextInt();
        }
        int min,max;
-       if(n==1){
+       if(n==1)
+       {
         min=a[0];
         max=a[0];
-        System.out.println("min is:"+min);
-        System.out.println("max is:"+max);
+        System.out.println("Min is: "+min);
+        System.out.println("Max is: "+max);
        }
        else if(n==2){
         min=a[0];
@@ -28,21 +29,23 @@ public class smlar {
             min=a[0];
             max=a[1];
         }
-        System.out.println("min is:"+min);
-        System.out.println("max is:"+max);
+        System.out.println("Min is: "+min);
+        System.out.println("Max is: "+max);
         }
         else{
             min=a[0];
             max=a[0];
-            for(i=1;i<n;i++){
+            for(i=1;i<n;i++)
+            {
             if(a[i]>max){
                 max=a[i];
             }
             if(a[i]<min){
                 min=a[i];
-            }}
-            System.out.println("min is:"+min);
-            System.out.println("max is:"+max);
+            }
+            }
+            System.out.println("Min is: "+min);
+            System.out.println("Max is: "+max);
         }
     }
 }
