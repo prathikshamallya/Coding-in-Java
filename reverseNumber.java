0308f77bd606de19c9457e70bb1f8af2f9e17585
PrathikshaMallya <prathikshamallya12@gmail.com>
2024-03-05 22:16:40 +0530

    reverse a number

---
+++ reverseNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n=sc.nextInt();
+        System.out.println("The number before reverse is: "+n);
+        int rem=0,rev=0;
+        System.out.print("The number After reverse is: ");
+        while(n!=0){
+            rem=n%10;
+            rev=rev*10+rem;
+            // System.out.print(rem);
+            n=n/10;
+        }
+        System.out.print(rem);
+    }
+}
